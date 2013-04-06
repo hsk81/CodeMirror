1ea4a4a80ff6de53859b3f088734faf3b108c502
Hasan Karahan <hasan.karahan81@gmail.com>
2013-04-06 22:59:47 +0500

    Merged `yaml-plus` mode into `yaml`

--- mode/yaml/yaml.js
+++ mode/yaml/yaml.js
@@ -1,4 +1,4 @@
-CodeMirror.defineMode("yaml", function() {
+CodeMirror.defineMode("yaml-base", function() {
 
   var cons = ['true', 'false', 'on', 'off', 'yes', 'no'];
   var keywordRegex = new RegExp("\\b(("+cons.join(")|(")+"))$", 'i');
@@ -93,5 +93,59 @@ CodeMirror.defineMode("yaml", function() {
     }
   };
 });
+
+CodeMirror.defineMode('yaml', function (config, parserConfig) {
+
+    var TAG_DOLLAR = 'meta';
+    var TAG_BRACKET = 'bracket';
+    var TAG = 'atom';
+
+    var overlay = {
+        startState: function () {
+            return {tags: [], nob: 0};
+        },
+
+        token: function (stream, state) {
+
+            if (stream.match('${')) {
+                state.tags.push(TAG_DOLLAR);
+                stream.backUp(1);
+                return TAG_DOLLAR;
+            }
+
+            if (stream.match('{')) {
+                var lhs = state.tags.pop();
+                if (lhs == TAG_DOLLAR) {
+                    state.tags.push(TAG_BRACKET);
+                    state.nob++;
+                    return TAG_BRACKET;
+                } else {
+                    state.tags.push(lhs, null);
+                    return null;
+                }
+            }
+
+            if (stream.match('}')) {
+                var rhs = state.tags.pop();
+                if (rhs == TAG_BRACKET) state.nob--;
+                return rhs;
+            }
+
+            while (stream.next() != null) {
+                if (stream.match('${', false)) break;
+                if (stream.match('{', false)) break;
+                if (stream.match('}', false)) break;
+            }
+
+            return (state.nob > 0) ? TAG : null;
+        }
+    };
+
+    var mode = CodeMirror.getMode(
+        config, parserConfig.backdrop || 'yaml-base'
+    );
+
+    return CodeMirror.overlayMode(mode, overlay);
+}, 'yaml-base');
 
 CodeMirror.defineMIME("text/x-yaml", "yaml");
